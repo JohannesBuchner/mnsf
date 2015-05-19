8a6260689a712df702e8e3efa0f73c0c94366883
J. Michael Burgess <jmichaelburgess@gmail.com>
2015-05-19 07:16:49 +0200

    added more example stuffs

--- mnSpecFit/models/SBPL.py
+++ mnSpecFit/models/SBPL.py
@@ -13,35 +13,35 @@ class SBPL(Model):
 
         def sbpl(ene, logN, indx1, breakE, breakScale, indx2):
 
-        pivot =300. #keV
-        B = (indx1 + indx2)/2.0
-        M = (indx2 - indx1)/2.0
+            pivot =300. #keV
+            B = (indx1 + indx2)/2.0
+            M = (indx2 - indx1)/2.0
 
-        arg_piv = log10(pivot/breakE)/breakScale
+            arg_piv = log10(pivot/breakE)/breakScale
 
-        if arg_piv < -6.0:
-            pcosh_piv = M * breakScale * (-arg_piv-log(2.0))
-        elif arg_piv > 4.0:    
-            
-            pcosh_piv = M * breakScale * (arg_piv - log(2.0))
-        else:
-            pcosh_piv = M * breakScale * (log( (exp(arg_piv) + exp(-arg_piv))/2.0 ))
+            if arg_piv < -6.0:
+                pcosh_piv = M * breakScale * (-arg_piv-log(2.0))
+            elif arg_piv > 4.0:    
 
-            
-        arg = numexpr.evaluate('log10(ene/breakE)/breakScale')
-        idx1 =  arg < -6.0
-        idx2 =  arg >  4.0
-        idx3 =  ~logical_or(idx1,idx2)
+                pcosh_piv = M * breakScale * (arg_piv - log(2.0))
+            else:
+                pcosh_piv = M * breakScale * (log( (exp(arg_piv) + exp(-arg_piv))/2.0 ))
 
-        pcosh = zeros(ene.flatten().shape[0])
-        
-        pcosh[idx1] = (-arg[idx1]-log(2.0))
-        pcosh[idx2] = (arg[idx2] - log(2.0))
-        pcosh[idx3] = (log( (exp(arg[idx3]) + exp(-arg[idx3]))/2.0 ))
 
-        return numexpr.evaluate( 'M * breakScale * 10.**logN * (ene/pivot)**B * 10.**(pcosh-pcosh_piv) ')
+            arg = numexpr.evaluate('log10(ene/breakE)/breakScale')
+            idx1 =  arg < -6.0
+            idx2 =  arg >  4.0
+            idx3 =  ~logical_or(idx1,idx2)
 
-        return val
+            pcosh = zeros(ene.flatten().shape[0])
+
+            pcosh[idx1] = (-arg[idx1]-log(2.0))
+            pcosh[idx2] = (arg[idx2] - log(2.0))
+            pcosh[idx3] = (log( (exp(arg[idx3]) + exp(-arg[idx3]))/2.0 ))
+
+            return numexpr.evaluate( 'M * breakScale * 10.**logN * (ene/pivot)**B * 10.**(pcosh-pcosh_piv) ')
+
+
 
 
 
